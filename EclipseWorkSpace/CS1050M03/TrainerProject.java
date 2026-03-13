0afa4181cf6467662c69ea0181e5821e28f96232
vickk05 <kr5h9drgyx@privaterelay.appleid.com>
2026-03-12 21:15:17 -0600

    BMI

---
+++ EclipseWorkSpace/CS1050M03/TrainerProject.java
@@ -0,0 +1,20 @@
+
+public class TrainerProject {
+
+	public static void main(String[] args) {
+		System.out.println("Team and Athlete Analysis");
+		System.out.println("The trainer enters athlete data until they indicate they are done entering data. The trainer will enter each athlete’s weight and height");
+		System.out.println("For each athlete entered, the BMI value and category will be displayed based on these BMI ranges.");
+		System.out.println("Under 18.5: Underweight");
+		System.out.println("18.5 to under 25: Normal");
+		System.out.println("25 to under 30: Overweight");
+		System.out.println("30 or greater: Obese ");
+
+
+		
+		
+		
+		
+	}
+
+}
